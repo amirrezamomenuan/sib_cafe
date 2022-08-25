3f4dfb79fa8003c092b0043dffdd3f547b95da76
reza eivazzadeh <amirrezamomenian@gmail.com>
2022-08-26 00:24:10 +0430

    Food model tests completed

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -8,10 +8,62 @@ class FoodTests(TestCase):
             description = "warm food containing meat & rice",
             category = Food.foodCategories.LUNCH,
         )
+        Food.objects.create(
+            name = 'ghoremeh',
+            description = "warm food containing meat & rice & sabzi",
+            category = Food.foodCategories.LUNCH,
+        )
+        Food.objects.create(
+            name = 'koobideh kabaab',
+            description = "meat and fat and onion cooked on fire",
+            category = Food.foodCategories.LUNCH,
+        )
+        Food.objects.create(
+            name = 'fries',
+            description = "fried potatos and salt",
+            category = Food.foodCategories.APPETIZER,
+        )
+        Food.objects.create(
+            name = 'soda',
+            category = Food.foodCategories.APPETIZER,
+        )
+        Food.objects.create(
+            name = 'souffle',
+            description = "luxury dish for employee of month",
+            category = Food.foodCategories.BREAKFAST,
+        )
+        Food.objects.create(
+            name = 'cesar salad',
+            description = "salad with extra bs",
+            category = Food.foodCategories.APPETIZER,
+        )
+        Food.objects.create(
+            name = 'english breakfast',
+            description = "fatty and unhealthy breakfast containing bacon and egs and beans",
+            category = Food.foodCategories.BREAKFAST,
+        )
     
     def test_category(self):
         gheymeh = Food.objects.get(name="gheymeh")
-        self.assertEqual(gheymeh.category, 'L')
+        ghoremeh = Food.objects.get(name="ghoremeh")
+        koobideh = Food.objects.get(name="koobideh kabaab")
+        souffle = Food.objects.get(name="souffle")
+        soda = Food.objects.get(name="soda")
+        fries = Food.objects.get(name="fries")
+        english_breakfast = Food.objects.get(name="english breakfast")
 
-    def test_2(self):
-        pass
+        #assertions
+        self.assertEqual(gheymeh.category, Food.foodCategories.LUNCH.value)
+        self.assertEqual(ghoremeh.category, Food.foodCategories.LUNCH.value)
+        self.assertEqual(koobideh.category, Food.foodCategories.LUNCH.value)
+        
+        self.assertEqual(souffle.category, Food.foodCategories.BREAKFAST.value)
+        self.assertEqual(english_breakfast.category, Food.foodCategories.BREAKFAST.value)
+
+        self.assertEqual(soda.category, Food.foodCategories.APPETIZER.value)
+        self.assertEqual(fries.category, Food.foodCategories.APPETIZER.value)
+
+    def test_ordering(self):
+        foods = Food.objects.all()
+        self.assertEqual(foods.first().name, 'english breakfast')
+        self.assertEqual(foods.last().name, 'gheymeh')
