e757b919f254e1ae93b58be1aed013a24fe9385b
reza eivazzadeh <amirrezamomenian@gmail.com>
2022-08-26 00:25:06 +0430

    debuging db_name and media and static file settings

--- core/models.py
+++ core/models.py
@@ -12,7 +12,7 @@ class Food(models.Model):
     name = models.CharField(verbose_name= _("name"), max_length =50)
     description = models.TextField(verbose_name=_('descriptions'), null=True, blank= True, max_length=300)
     category = models.CharField(_('category'), max_length=1, choices=foodCategories.choices)
-    image = models.ImageField(verbose_name=_('image'), upload_to ='/uploads/food_images', default='/uploads/food_images/default.jpg')
+    image = models.ImageField(verbose_name=_('image'), upload_to ='uploads/food_images', default='uploads/food_images/default.jpg')
     creation_time = models.DateTimeField(auto_now_add=True)
     modification_time = models.DateTimeField(auto_now=True)
 
@@ -20,14 +20,14 @@ class Food(models.Model):
         db_table = "core_food"
         verbose_name = _("food")
         verbose_name_plural = _('foods')
-        ordering = ["-creation_time", 'price']
+        ordering = ["-creation_time",]
     
     @property
     def rate(self):
         return 5 #TODO: change this to a function that reads data from redis and returns it
     
     @rate.setter
-    def rate(self, rate:float) -> None:
+    def rate(self) -> None:
         pass #TODO: change this to a function that updates new data to redis
 
     def __str__(self) -> str:
--- sib_cafe/settings.py
+++ sib_cafe/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config, Csv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,5 +83,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    (os.path.join(BASE_DIR, 'static')),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
