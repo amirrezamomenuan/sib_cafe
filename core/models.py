9c8d989f1eaf5d4c3d6ee90b0d945352beb55c2d
reza eivazzadeh <amirrezamomenian@gmail.com>
2022-08-25 21:12:54 +0430

    added food model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Food(models.Model):
+    name = models.CharField(verbose_name= _("name"), max_length =50)
+    description = models.TextField(verbose_name=_('descriptions'), null=True, blank= True, max_length=300)
+    category = models.CharField(_('category'), max_length=1)
+    image = models.ImageField(verbose_name=_('image'), upload_to ='/uploads/food_images', default='/uploads/food_images/default.jpg')
+    creation_time = models.DateTimeField(auto_now_add=True)
+    modification_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "core_food"
+        verbose_name = _("food")
+        verbose_name_plural = _('foods')
+        ordering = ["-creation_time", 'price']
