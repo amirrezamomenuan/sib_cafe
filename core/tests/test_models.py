6634a4df39b014b3830b7dc0d6800668ba293b37
reza eivazzadeh <amirrezamomenian@gmail.com>
2022-08-25 23:18:30 +0430

    starting to write tests

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -1,1 +1,12 @@
 from django.test import TestCase
+
+
+class FoodTests(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def test_1(self):
+        pass
+
+    def test_2(self):
+        pass
