ae68fe5d9fa852624fa2cd69fe9b22dccd5fbb87
reza eivazzadeh <amirrezamomenian@gmail.com>
2022-08-27 00:20:24 +0430

    added fooditem admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from core.models import Food
+from core.models import Food, FoodItem
 
 @admin.register(Food)
 class FoodAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FoodItem)
+class FoodItemAdmin(admin.ModelAdmin):
     pass
