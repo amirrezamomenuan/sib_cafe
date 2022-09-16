65ed840b7eef6399bf77fa99ad7362c5d0ae678e
reza eivazzadeh <amirrezamomenian@gmail.com>
2022-09-16 20:59:30 +0430

    cleaned core.models.FoodItem

--- core/models.py
+++ core/models.py
@@ -63,42 +63,12 @@ class FoodItem(models.Model):
 
     def __food_weekday_matches_order_date(self, order_date):
         if self.weekday == -1:
-            print('daily food can be ordered')
             return True
-        print(f"\nweekday for order_date is {(order_date.weekday() + 2) % 7}, self.weekday is {self.weekday}")
         return self.weekday == (order_date.weekday() + 2) % 7
 
     def __user_has_ordered_limited_food(self, user, food_item_id, order_date:date):
-        print("\nchecking if user has ordered this food\n")
         return user.user_orders.filter(food_item__id=food_item_id, order_date=order_date).exists()
 
-    def __user_can_order_breakfast(self, user, food_item_id, order_date:date) -> bool:
-        if datetime.now().hour >= settings.BREAKFAST_TIME_LIMIT:
-            print("\nout of breakfast time limit\n")
-            return False
-        elif date.today() < order_date:
-            print(order_date)
-            print("\nout of breakfast date\n")
-            return False
-        elif not self.__food_weekday_matches_order_date(order_date):
-            print('food_item doesnot match weekday')
-            return False
-        return not self.__user_has_ordered_limited_food(user, food_item_id, order_date)
-    
-    def __user_can_order_lunch(self, user, food_item_id, order_date:date):
-        print('\n', datetime.now().hour, "is woooooooooooooooooorking\n")
-        if datetime.now().hour >= settings.LUNCH_TIME_LIMIT:
-            print("\nout of lunch time limiit\n")
-            return False
-        elif date.today() <= order_date:
-            print(order_date, ' ++++++ ', date.today())
-            print("\nout of lunch date\n")
-            return False
-        elif not self.__food_weekday_matches_order_date(order_date):
-            print('food_item doesnot match weekday')
-            return False
-        return not self.__user_has_ordered_limited_food(user, food_item_id, order_date)
-    
     def __check_time_limit(self):
         if self.food.category == Food.foodCategories.BREAKFAST.value:
             return datetime.now().hour < settings.BREAKFAST_TIME_LIMIT
@@ -109,10 +79,7 @@ class FoodItem(models.Model):
     def __order_time_is_over(self, order_date:date):
         date_difference = (order_date - date.today()).days
         if date_difference >= 7:
-            print('order is too far')
             return True
-        print(f"\ndate difference is : {date_difference}\n")
-        print('order date for current request is: ', order_date, 'todays date is : ', date.today())
 
         if self.food.category == Food.foodCategories.BREAKFAST.value:
             if date_difference == 0:
@@ -130,57 +97,41 @@ class FoodItem(models.Model):
         return True
 
     def __user_can_order_limited_food(self, user, food_item_id, order_date):
-        print('\n', datetime.now().hour, "is woooooooooooooooooorking\n")
         if self.__order_time_is_over(order_date):
-            print("\nout of order time limit\n")
             return False
         elif not self.__food_weekday_matches_order_date(order_date):
-            print('food_item doesnot match weekday')
             return False
         return not self.__user_has_ordered_limited_food(user, food_item_id, order_date)
 
     def __user_can_order_item(self, user, food_item_id, order_date:date) -> bool:
         if self.food.category == Food.foodCategories.APPETIZER.value:
-            print("\nordering a breakfast\n")
             return not self.__order_time_is_over(order_date)   
         return self.__user_can_order_limited_food(user, food_item_id, order_date)
             
     def __food_is_sold_out(self) -> bool:
-        print("checking redis in here")
         if self.amount is None:
             return False
-        s_time = time()
         try:
             ordered_food_count = settings.REDIS_CONNECTION.get(f'{self.pk}')
             if ordered_food_count is None:
-                print('no data is available in redis for key: ', self.pk)
                 raise Exception('key for order_item.pk is not provided')
             ordered_food_count = int(ordered_food_count.decode('utf-8'))
-            print('redis is working fine and the food_count is: ', ordered_food_count)
+            
         except Exception as e:
-            print("\n hitted 11111111  first 111111111 exception in redis\n")
-            print(f'first error message is {e}')
             today = date.today()
             ordered_food_count = self.food_orders.filter(order_date=today).count()
             try:
                 settings.REDIS_CONNECTION.set(name=f'{self.pk}', value=ordered_food_count)
-                print('added new value to redis: ', ordered_food_count)
             except:
-                print("\n hitted 22222222222222222 exception in redis\n")
-                print("failed to add new value to redis")
+                pass
 
-        print('checking redis completed')
-        print(self.amount, " -------- ", ordered_food_count)
-        print(f'\n took {time() - s_time} to complete redis query\n')
-        if self.amount == ordered_food_count:
-            print('======food is sold out======')
         return self.amount <= ordered_food_count
     
     def can_be_ordered(self, user, order_date:date) -> bool:
         user_can_order = self.__user_can_order_item(user, self.pk, order_date)
-        print("\nuser can order food: ", user_can_order)
-        sold_out = self.__food_is_sold_out()
-        return user_can_order and not sold_out
+        if not user_can_order:
+            return False
+        return user_can_order and not self.__food_is_sold_out()
 
     def __str__(self) -> str:
         return f"{self.food.name}: {self.price}"
